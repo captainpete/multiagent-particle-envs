a1844af13e160a97fac2e5bde353daee068d1289
Peter Hollows <github@dojo7.com>
2020-11-28 22:36:52 +1100

    keep all snapshots

--- experiments/train.py
+++ experiments/train.py
@@ -85,7 +85,7 @@ def train(arglist):
         obs_shape_n = [env.observation_space[i].shape for i in range(env.n)]
         num_adversaries = min(env.n, arglist.num_adversaries)
         trainers = get_trainers(env, num_adversaries, obs_shape_n, arglist)
-        saver = tf.train.Saver()
+        saver = tf.train.Saver(max_to_keep=None)
         print('Using good policy {} and adv policy {}'.format(arglist.good_policy, arglist.adv_policy))
 
         # Initialize
@@ -183,7 +183,7 @@ def train(arglist):
                     # save policy snapshot
                     snapshot_folder = "{}/{}".format(arglist.save_dir, arglist.exp_name)
                     os.makedirs(snapshot_folder, exist_ok=True)
-                    saver.save(U.get_session(), snapshot_folder)
+                    saver.save(U.get_session(), snapshot_folder + '/session', global_step=episode_number)
                     # save rewards
                     rew_file_name = arglist.plots_dir + arglist.exp_name + '_rewards.pkl'
                     with open(rew_file_name, 'wb') as fp:
